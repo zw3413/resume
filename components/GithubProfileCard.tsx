4643ddcf6ce5991c22bdb1d3120f9a632784e309
Elvin Zhang <zhangweicalm@gmail.com>
2025-06-07 14:39:18 -0400

    update the slogan

--- components/GithubProfileCard.tsx
+++ components/GithubProfileCard.tsx
@@ -19,10 +19,10 @@ const GithubProfileCard = ({ avatar_url, bio, location }: GithubUserType) => {
             </Col>
             <Col lg="8" className="order-lg-1">
              
-              <p className="text-white mt-3">Are you looking for a versatile individual who can handle things independently?</p>
+              <p className="text-white mt-3">Are you looking for a versatile individual who takes initiative?</p>
               {/* <p className="text-white mt-3">Or do you want someone to discuss your interesting ideas with?</p>
               <p className="text-white mt-3">Do you need someone who can quickly delivery value for your clients?</p> */}
-              <p className="text-white mt-3">Reach out to me.</p>
+              {/* <p className="text-white mt-3">Reach out to me.</p> */}
 
               {/* <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
                 <i className="ni ni-pin-3 text-info mr-2" />
