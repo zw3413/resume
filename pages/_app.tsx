fd5017a1bd97e24fb3e064ae051b889f8b28a6da
Elvin Zhang <zhangweicalm@gmail.com>
2025-06-08 14:55:54 -0400

    fix font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/argon-design-system-react.css";
 //import "../styles/black-dashboard/assets/css/black-dashboard.css";
 import "../styles/styles.css";
-import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
 import '../styles/custom.css';
 
