68a74afc4c19c55172fd84b4e945ec02e72a013c
Elvin Zhang <zhangweicalm@gmail.com>
2025-06-08 14:50:16 -0400

    recovery font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "next/app";
-import Head from 'next/head';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/argon-design-system-react.css";
 //import "../styles/black-dashboard/assets/css/black-dashboard.css";
@@ -7,18 +6,9 @@ import "../styles/styles.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
 import '../styles/custom.css';
-import '../styles/fonts.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
