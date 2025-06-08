7f1b95516172d335d12a710561bff35adf77c140
Elvin Zhang <zhangweicalm@gmail.com>
2025-06-08 14:45:19 -0400

    fix font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Head from 'next/head';
+import { JetBrains_Mono } from 'next/font/google';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/argon-design-system-react.css";
 //import "../styles/black-dashboard/assets/css/black-dashboard.css";
@@ -8,21 +9,29 @@ import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
 import '../styles/custom.css';
 
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+  display: 'swap',
+  variable: '--font-jetbrains-mono',
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap"
-          rel="stylesheet"
-        />
         <style jsx global>{`
+          :root {
+            --font-jetbrains-mono: ${jetbrainsMono.style.fontFamily};
+          }
           * {
-            font-family: 'JetBrains Mono', 'Menlo', 'Meslo LG', monospace !important;
+            font-family: var(--font-jetbrains-mono), 'Menlo', 'Meslo LG', monospace !important;
           }
         `}</style>
       </Head>
-      <Component {...pageProps} />
+      <main className={jetbrainsMono.className}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
