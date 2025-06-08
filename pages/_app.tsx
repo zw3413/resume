6e359bb606e45764f427f4fd528e30e9cb5c0fbb
Elvin Zhang <zhangweicalm@gmail.com>
2025-06-08 14:42:53 -0400

    fix font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Head from 'next/head';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/argon-design-system-react.css";
 //import "../styles/black-dashboard/assets/css/black-dashboard.css";
@@ -8,7 +9,22 @@ import "../styles/vendor/nucleo/css/nucleo.css";
 import '../styles/custom.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        <style jsx global>{`
+          * {
+            font-family: 'JetBrains Mono', 'Menlo', 'Meslo LG', monospace !important;
+          }
+        `}</style>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
