f73f32aae4dea96ebb98e60f21574e56ecb5b98c
Elvin Zhang <zhangweicalm@gmail.com>
2025-06-08 14:47:33 -0400

    fix font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from "next/app";
 import Head from 'next/head';
-import { JetBrains_Mono } from 'next/font/google';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/argon-design-system-react.css";
 //import "../styles/black-dashboard/assets/css/black-dashboard.css";
@@ -8,30 +7,16 @@ import "../styles/styles.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
 import '../styles/custom.css';
-
-const jetbrainsMono = JetBrains_Mono({
-  subsets: ['latin'],
-  weight: ['400', '500', '600'],
-  display: 'swap',
-  variable: '--font-jetbrains-mono',
-});
+import '../styles/fonts.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <style jsx global>{`
-          :root {
-            --font-jetbrains-mono: ${jetbrainsMono.style.fontFamily};
-          }
-          * {
-            font-family: var(--font-jetbrains-mono), 'Menlo', 'Meslo LG', monospace !important;
-          }
-        `}</style>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
-      <main className={jetbrainsMono.className}>
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </>
   );
 }
